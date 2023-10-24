c39b1ac2d1b2cfe7eecb7b0283d5fa8539625c73
DanTCTA <daniil.here@gmail.com>
2023-10-24 14:20:14 -0700

    First Pass, may be buggy
    (Currently doesn't change print behavior)

--- fullprint.py
+++ fullprint.py
@@ -3,8 +3,21 @@ import os
 # Older version, new one got deleted
 def FullPrint(*args, end="\n"):
     text = ""
+    tLen = 0
+    nText = ""
+    word = ""
+    termLen = os.get_terminal_size().columns # termLen => Terminal Length
     for i in args:
         text += str(i) + " "
-    SpaceNum = os.get_terminal_size().columns - 1
-    print(" " * SpaceNum, end = "\r")
+    for i in text:
+        word += i
+        tLen += 1
+        if i == " ":
+            if tLen >= termLen:
+                print(nText)
+                tLen = len(word)
+                nText = word
+            else:
+                nText += word
+    print(" " * termLen - 1, end = "\r")
     print(text, end=end)
