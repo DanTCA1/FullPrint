3c39d6a977de2fcf4c40b891f37efbb81d37685e
DanTCTA <daniil.here@gmail.com>
2023-11-03 17:02:38 -0700

    Pass 4: Space handling
    - Fixed issue with spaces between lines
    - Better implementation of space handling
    - Added more comments

--- fullprint.py
+++ fullprint.py
@@ -2,7 +2,7 @@ import os, sys, re, ctypes
 from ctypes import wintypes
 
 def cursorPos():
-    # Credits to https://stackoverflow.com/questions/35526014/
+    # Credits to https://stackoverflow.com/questions/35526014/ for the cursor detection script
     OldStdinMode = ctypes.wintypes.DWORD()
     OldStdoutMode = ctypes.wintypes.DWORD()
     kernel32 = ctypes.windll.kernel32
@@ -26,34 +26,40 @@ def cursorPos():
 
 def fullPrint(*args, end="\n"):
     x, y = cursorPos()
-    if x != -1:
+    if x != -1: # First detection of where the cursor is
         tLen = x - 1
     else:
         tLen = 0
     text = ""
-    nText = ""
+    nText = "" # nText => New Text
     word = ""
     termLen = os.get_terminal_size().columns # termLen => Terminal Length
-    for i in args:
+    for i in args: # Compile all the print parameters into one string
         text += str(i) + " "
     text = text[:-1]
     for i in text:
-        word += i
-        if i == "\t":
-            tLen += 4
+        if i != " ":
+            word += i
+            if i == "\t": # Shorter chars are fine because the cursor script will deal with it
+                tLen += 4
+            else:
+                tLen += 1
         else:
-            tLen += 1
-        if i == " ":
-            if tLen >= termLen + 1: # I think this works because the space adds a char
+            # Triggered every word, if the word is too long, its put on the next line
+            if tLen >= termLen:
                 print(nText)
                 tLen = len(word)
                 nText = word
                 word = ""
             else:
-                nText += word
+                tLen += 1
+                nText += word + " "
                 word = ""
-    
-    if tLen >= termLen + 1:
+
+    if nText.endswith(" ") and len(word) == 0: # If a space is between 2 lines, its ignored because it could be printed later
+        nText = nText[:-1]
+        tLen -= 1
+    if tLen >= termLen:
         print(nText)
         print(word, end="")
     else:
