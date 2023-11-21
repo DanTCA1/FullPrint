0fe3c3c7f6aa5d3270f0b11014ddef0a2802c717
DanTCTA <daniil.here@gmail.com>
2023-11-20 22:41:08 -0800

    Pass 5: \r + line handling
    - Added support to using the \n operator within fullPrint
    - Fixed edge case where a word was as long as the entire line
    - Fixed an issue where spaces right after a line change was deleted
    - Added support top using \r as a way to return tpo the beginning of the string
    - Added a line count printed return
    - Added more documentation to non-obvious code
    - Removed code that "Fixed issue with spaces between lines" as it wasn't apparent what it did

--- fullprint.py
+++ fullprint.py
@@ -25,6 +25,14 @@ def cursorPos():
     return (-1, -1)
 
 def fullPrint(*args, end="\n"):
+    """
+    Acts like a normal print operator, with infinite args, and an end value, but supports multi-line operations.
+
+    Params: *args, end="\\n"
+    Returns: Line count printed (1 based index)
+
+    An end of "\\r" will return to the beginning of the string, even if it spanned multiple lines
+    """
     x, y = cursorPos()
     if x != -1: # First detection of where the cursor is
         tLen = x - 1
@@ -33,12 +41,25 @@ def fullPrint(*args, end="\n"):
     text = ""
     nText = "" # nText => New Text
     word = ""
+    lines = 1
     termLen = os.get_terminal_size().columns # termLen => Terminal Length
+
     for i in args: # Compile all the print parameters into one string
         text += str(i) + " "
     text = text[:-1]
     for i in text:
-        if i != " ":
+        if i == "\n": # Lines are added with \n
+            if tLen >= termLen:
+                print(nText)
+                print(word)
+                lines += (len(nText + word) // termLen) + 1
+            else:
+                print(nText + word)
+                lines += 1
+            nText = ""
+            word = ""
+            tLen = 0
+        elif i != " ":
             word += i
             if i == "\t": # Shorter chars are fine because the cursor script will deal with it
                 tLen += 4
@@ -47,21 +68,30 @@ def fullPrint(*args, end="\n"):
         else:
             # Triggered every word, if the word is too long, its put on the next line
             if tLen >= termLen:
-                print(nText)
-                tLen = len(word)
-                nText = word
+                lines += (len(nText) // termLen) # If a word is long enough, it could take up multiple lines. The entire word is still printed, without any \n incase the terminal becomes wider
+                if len(nText) == 0: # Case where the one word is the only thing on the whole line
+                    print(word + " ")
+                    tLen = 0
+                    nText = ""
+                else:
+                    print(nText)
+                    tLen = len(word)
+                    nText = word + " "
                 word = ""
             else:
                 tLen += 1
                 nText += word + " "
                 word = ""
 
-    if nText.endswith(" ") and len(word) == 0: # If a space is between 2 lines, its ignored because it could be printed later
-        nText = nText[:-1]
-        tLen -= 1
     if tLen >= termLen:
         print(nText)
         print(word, end="")
     else:
         print(nText + word, end="")
+
+    lines += (len(nText) // termLen)
+    if end.find("\r") + 1:
+        for _ in range(lines - 1):
+            print("\033[F", end="")
     print(end, end="")
+    return(lines)
