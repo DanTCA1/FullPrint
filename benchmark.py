e64996cb8562fd1f9cd55cf14413240710d8803b
DanTCTA <daniil.here@gmail.com>
2024-03-31 22:15:00 -0700

    Debug features
    - Added a gitignore so you can upload to github without deleting a cache
    - Added a benchmarking tool to test the speed of print vs fullPrint
    
    Current stats for benchmark:
    - Empty: 30k ops. VS 57k ops.
    - Small: 26k ops. VS 54k ops.
    - Large: 10k ops. VS 50k ops.
    - SpaceBar Spam: 24k ops. VS 55k ops.
    - \n Spam: 3k ops. VS 21k ops.

---
+++ benchmark.py
@@ -0,0 +1,40 @@
+import fullPrint, time
+
+testType = {
+    "Empty":"",
+    "Small":"this is a small sentence",
+    "Large":"This is a sentence that should span multiple lines if I coded it correctly which I think I did, but I might not have, I don't know This is another sentence that should span multiple lines if I coded it correctly which I think I did, but I might not have, I don't know pt 2",
+    "SpaceBar Spam":"h  i     t  h  e  r  e     h  o  w     a  r  e     y  o  u  ?",
+    "\\n Spam":"h\ni\n \nt\nh\ne\nr\ne\n \nh\no\nw\n \na\nr\ne\n \ny\no\nu\n?",
+}
+
+printTime = 1
+results = []
+for text in testType.values():
+    lines = fullPrint.fullPrint(text)
+    print(fullPrint.up() * lines, end="")
+    up = fullPrint.up() * lines
+    fullCount = 0
+    startTime = time.time()
+    while time.time() <= startTime + printTime:
+        for _ in range(1000):
+            fullPrint.fullPrint(text)
+            print(up, end="")
+        fullCount += 1
+    
+    printCount = 0
+    startTime = time.time()
+    while time.time() <= startTime + printTime:
+        for _ in range(1000):
+            print(text)
+            print(up, end="")
+        printCount += 1
+    
+    results.append((fullCount, printCount))
+
+print("\n" * 20)
+print("Test results (fullPrint vs print):")
+i = 0
+for test in testType.keys():
+    print(f"{test}: {results[i][0]}k ops. VS {results[i][1]}k ops.")
+    i+= 1
