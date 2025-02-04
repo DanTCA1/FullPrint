a97a9fe5efe12593d6d588857c08f154c03d0ca2
DanTCA <daniil.here@gmail.com>
2025-02-03 16:43:29 -0800

    Pass 9: Refactor code, add ANSI support, improve QoL and documentation
    fullPrint (248 => 366)
    
    - Fix `up()` command to use a better ANSI command that works offscreen
    - Add PEP 483 compliant type hints
    - Rename `cursorPos` to `_cursorPos` to indicate private method
    - Change function descriptions to be bulleted instead of paragraphed
    - Remove `SetPrintMode` and all associated overrides and comments
    - Change function descriptions to be more concise
    - Add `sep` to replicate print
    - Add `setDefaults` to set defaults for `fullPrint` such as `sep`, `end`, etc.
    - Add `rmExtraChars`, `cursorCheck`, and `maintainWords` as params to `fullPrint`
      - `rmExtraChars`: Uses \033[0K to clear each line after printing
      - `cursorCheck`: Make cursor checking optional
      - `maintainWords`: Maintain words in the string when they span multiple lines
    - Change function to object containing `x` and `y` (offset vertical and horizontal)
    - Implement cursor checking in case the string printed is smaller than assumed (ANSI for ex.)
    - Add an error raise during \n processing to prevent infinite loops
    - Add terminal length prints to all infinite loop raises for easier debugging
    - Add regions to the code to make it easier to read
    - Fix end \r being skipped while \n was being processed
    - Improve space processing in general
    - Fix multiple edge cases such as when word was length of terminal
    - Add a demo file to show off features
      - Show off `repeatPattern`
      - Show off `fullPrint` \r capabilities
      - Show off extra character removal (`rmExtraChars`)
    
    TODO:
    - Use more `cursorCheck` to help with space processing edge cases
    - Add more to the demo file
    - Make an example folder to show off use cases of `fullPrint`

--- fullPrint.py
+++ fullPrint.py
@@ -1,14 +1,22 @@
-import os, sys, re
+import os
+import sys
+import re
+import types
+from typing import Literal
 if(sys.platform == "win32"):
     import ctypes
     from ctypes import wintypes
 else:
     import termios
 
-mode = 3
 relConsolePos = -1 # relConsolePos => relative console position
+sepDefault = " "
+endDefault = "\n"
+rmExtraCharsDefault = True
+cursorCheckDefault = False
+maintainWordsDefault = True
 
-def cursorPos():
+def _cursorPos():
     # Credits to https://stackoverflow.com/questions/35526014/ for the cursor detection script
     if(sys.platform == "win32"):
         OldStdinMode = ctypes.wintypes.DWORD()
@@ -44,20 +52,29 @@ def up():
     """
     Returns an ANSI sequence for going one line up in console
     """
-    return "\033[F"
+    return "\033M"
 
-def repeatPattern(pattern, end="", fitPattern=True, printPattern=False):
-    """
-    Prints a pattern across the whole width of the console
+def repeatPattern(
+    pattern: str,
+    end: str="",
+    fitPattern: bool=True,
+    printPattern=False
+) -> str | None:
+    """Prints a pattern across the whole width of the console
 
     FitPattern options:
-    True : repeat until pattern won't fit
-    |-=-=-=-=-= |
-    False : repeat until entire console is filled (Doesn't work with fitPattern)
-    |-=-=-=-=-=-|
 
-    printPattern=True will result in more consistent results if ANSI sequences are in the pattern
-    Returns string when printPattern = False, returns 1 when printPattern = True
+    True : repeat until pattern won't fit `|123,123, |`
+
+    False : repeat until entire console is filled (Doesn't work with end) `|123,123,1|`
+
+    Args:
+        pattern: Pattern to repeat
+        end: String appended after the pattern can no longer be repeated
+        fitPattern: Described above
+        printPattern: Prints the pattern, increases consistency if ANSI sequences are in the pattern
+    Returns:
+        String when printPattern = False, returns None when printPattern = True
     """
     fString = "" # fString => Final String
     termLen = os.get_terminal_size().columns # termLen => Terminal Length
@@ -72,12 +89,12 @@ def repeatPattern(pattern, end="", fitPattern=True, printPattern=False):
         endLen = len(end)
     else:
         print(pattern, end="")
-        x, _ = cursorPos()
+        x, _ = _cursorPos()
         patternLen = x - 1
         print("\r", end="")
 
         print(end, end="")
-        x, _ = cursorPos()
+        x, _ = _cursorPos()
         endLen = x - 1
         print("test\r", end="")
 
@@ -89,23 +106,22 @@ def repeatPattern(pattern, end="", fitPattern=True, printPattern=False):
     
     if printPattern == False:
         return fString
-    fullPrint(fString, modeOverride=0)
-    return 1
+    print(fString)
+    return None
 
-def saveConsolePos():
-    """
-    Saves the console position to be used later. 
+def saveConsolePos() -> None:
+    """Saves the console position to be used later. 
+
     Call loadConsolePos to go back to where this was called.
     """
     global relConsolePos
     relConsolePos = 0
 
-def loadConsolePos():
-    """
-    Loads the console position previously saved.
-    Call saveConsolePos to save a position, and use this to go to it
-
+def loadConsolePos() -> None:
+    """Loads the console position previously saved.
     Calling this function keeps the old save intact, for use later
+
+    Call saveConsolePos to save a position, and use this to go to it
     """
     global relConsolePos
     if relConsolePos >= 0:
@@ -115,62 +131,84 @@ def loadConsolePos():
                 print("\033[F", end="")
         saveConsolePos()
 
-
-def setPrintMode(newMode = -1):
+def setDefaults(
+    sep: str | None = " ",
+    end: str | None = "\n",
+    rmExtraChars: bool = True,
+    cursorCheck: bool = False,
+    maintainWords: bool = True
+) -> None:
+    """Sets the default values for the fullPrint function.
+    
+    Check fullPrint for more information on the parameters.
     """
-    Sets the mode of extra character removal for the fullPrint operator. Returns the current mode (new mode if it was just set).
-    NOTE: Only mess with this if you are using ANSI or other methods to change the background color, this will not have any effect otherwise.
+    global sepDefault, endDefault, rmExtraCharsDefault, cursorCheckDefault, maintainWordsDefault
+    sepDefault = sep
+    endDefault = end
+    rmExtraCharsDefault = rmExtraChars
+    cursorCheckDefault = cursorCheck
+    maintainWordsDefault = maintainWords
 
-    Available modes:
-    0 : Inactive (Turns extra character removal off)
-    1 : Start of line reset (resets ANSI, clears line, user data)
-    2 : End of line reset (prints user data, resets ANSI, clears end of line)
-    3 : End of line inherit (prints user data, clears end of line)
-    4 : Double print clear (prints ALL lines of user data, resets ANSI, prints spaces, prints user data again)
+def fullPrint(
+    *values: object,
+    sep: str | None = sepDefault,
+    end: str | None = endDefault,
+    rmExtraChars: bool = rmExtraCharsDefault,
+    cursorCheck: bool = cursorCheckDefault,
+    maintainWords: bool = maintainWordsDefault
+) -> object:
+    """Acts like a normal print operator, with infinite args, and an end value, but supports multi-line operations and ANSI tools.
 
-    Extra character removal is useful when printing on the same line multiple times, using \\r, as the previous text will not be erased automatically.
+    Args:
+        *values: Values to be printed.
+        sep: String inserted between values, default is " ".
+        end: String appended after the last value, default is "\\n".
+        rmExtraChars: Controls whether extra character removal is on or off (Check examples.py or below for more details). Defaults to on.
+        cursorCheck: Checks where the cursor is using \\x1b[6n (Check below for more details). This is only useful when ANSI sequences are present, or if characters disappear. Default is off.
+        maintainWords: If a word is longer then the terminal, selects whether to split the word or not with \\n. Default is on.
+    Returns:
+        y: Line count printed (How many lines down it went, 0 is returned if the print finished on the same line)
+        x: Offset of the last printed character (How many characters to the right the last printed character is from the start, None is returned if more than one line was printed)
+
+    ## Notes
+        - WARNING: Having cursorCheck set to true can have a detrimental effect to performance (3x-5x performance reduction)
+        - WARNING: Having maintainWords set to false can ruin ANSI sequences if they span multiple lines
+        - An end of "\\r" will return to the beginning of the string, even if it spanned multiple lines.
+        - Extra character removal is useful when making status bars, or when \\r is commonly used to reprint over existing text.
+        - CursorCheck is useful when sequences are printed that may disappear (like ANSI), or when characters are longer than 1 character (like \\t)
+        - CursorCheck being active WILL NOT fix any issues that are caused with ANSI strings alongside a word longer then the length of the terminal
+        - When maintainWords is true, words spanning multiple lines will wordwrap, and will expand when the terminal is resized
     """
-    global mode
-    if newMode in range(0,5):
-        mode = newMode
-    return mode
-
-def fullPrint(*args, end="\n", modeOverride=None):
-    """
-    Acts like a normal print operator, with infinite args, and an end value, but supports multi-line operations.
-
-    Params: *args, end="\\n", modeOverride
-    Returns: Line count printed (How many lines down it went, if it only moved right, not down, 0 is returned)
-
-    An end of "\\r" will return to the beginning of the string, even if it spanned multiple lines
-    """
-
-    if not modeOverride in range(0,5) and modeOverride != None:
-        raise Exception("ModeOverride not in range 0-5")
 
     # Var init.
-    global relConsolePos, mode
+    global relConsolePos
     charLeft = 0 # charLeft => characters left (in string)
     currentIdx = 0
     lines = 0
+    offset = 0
     text = ""
     termLen = os.get_terminal_size().columns + 1 # termLen => Terminal Length
+    rmChar = "\033[0K" if rmExtraChars else ""
 
 
-    for i in args: # Compile all the print parameters into one string
-        text += str(i) + " "
-    text = text[:-1]
+    for i in values: # Compile all the print parameters into one string
+        text += str(i) + sep
+    if len(sep) != 0: # Remove the last separator
+        text = text[:0 - len(sep)]
 
 
     if len(text + end) == 0: # If nothing is printed, don't run the expensive cursor script
-        return 0
+        return(types.SimpleNamespace(y=0, x=0))
 
-
-    x, _ = cursorPos()
+    x = -1
+    if cursorCheck:
+        x, _ = _cursorPos()
     if x != -1: # First detection of where the cursor is
         charLeft = termLen - (x - 1)
+        startPos = x - 1
     else:
         charLeft = termLen
+        startPos = 0
 
 
     backTriggered = True
@@ -182,47 +220,53 @@ def fullPrint(*args, end="\n", modeOverride=None):
 
 
     i = 0
+    endWordCheck = False # Special case used with ANSI and long words, to make a newline if no more words could fit on the current line 
     while i <= len(text) + 1:
         i += 1
-        if i > len(text):
-            raise Exception("Woops! fullPrint has entered an infinite loop! Please contact us if you see this error.")
+        if i > len(text) + 1:
+            raise Exception(
+                f"Woops! fullPrint has entered an infinite loop! Please contact us if you see this error. (Main process, {termLen})"
+            )
 
-        # XXX \n processing, Priority: Med. XXX
-        nPrint = "" # nPrint => \n print
-        while text.find("\n", currentIdx, min(currentIdx + charLeft, backIdx)) != -1:
-            downLoc = text.find("\n", currentIdx, min(currentIdx + charLeft, backIdx)) # downLoc => down location
-            nPrint += text[currentIdx : downLoc] + "\n"
-            currentIdx = downLoc + 1
+        # region XXX \n & \r processing, Priority: High XXX
+        qPrint = "" # qPrint => queue print
+        downLoc = text.find("\n", currentIdx, min(currentIdx + charLeft, backIdx + 1)) # downLoc => down location
+        backLoc = text.find("\r", currentIdx, min(currentIdx + charLeft, backIdx + 1)) # backLoc => back location
+        j = 0
+
+        while downLoc != -1 or backLoc != -1:
+            # Gets the location of a \n or \r, takes a slice from the beginning, to the \n, and then starts from there onward
+            j += 1
+            if j > len(text):
+                raise Exception(
+                    f"Woops! fullPrint has entered an infinite loop! Please contact us if you see this error. (\\n & \\r process, {termLen})"
+                )
+            endWordCheck = False
+            downLoc = downLoc if downLoc >= 0 else 999999
+            backLoc = backLoc if backLoc >= 0 else 999999
+            if downLoc < backLoc:
+                qPrint += text[currentIdx : downLoc] + rmChar + "\n"
+                lines += 1
+            else:
+                if backLoc == backIdx:
+                    qPrint += text[currentIdx : backLoc]
+                    currentIdx = backLoc
+                    charLeft = termLen
+                    break
+                else:
+                    qPrint += text[currentIdx : backLoc] + rmChar + "\r"
+            currentIdx = min(downLoc, backLoc) + 1
             charLeft = termLen
-            lines += 1
-            # Gets the location of a \n, takes a slice from the beginning, to the \n, and then starts from there onward
-        print(nPrint, end="") # Text is compiled and then printed afterwards because calling print is s l o w
+            downLoc = text.find("\n", currentIdx, min(currentIdx + charLeft, backIdx + 1))
+            backLoc = text.find("\r", currentIdx, min(currentIdx + charLeft, backIdx + 1))
+        
+        if len(qPrint) != 0:
+            offset = None
+            print(qPrint, end="") # Text is compiled and then printed afterwards because calling print is s l o w
 
-        # XXX Space processing, Priority: Low XXX
-        spaceLoc = text.rfind(" ", currentIdx, min(currentIdx + charLeft, backIdx))
-        if spaceLoc != -1:
-            print(text[currentIdx : spaceLoc])
-            currentIdx = spaceLoc + 1
-            charLeft = termLen
-            lines += 1
-            # Gets the location of the last space on the line, and moves the word to the next one
-        else:
-            spaceLoc = text.find(" ", currentIdx, min(len(text), backIdx))
-            if spaceLoc == -1:
-                downLoc = text.find("\n", currentIdx, len(text))
-                if downLoc == -1:
-                    downLoc = 999999
-                spaceLoc = min(downLoc, len(text), backIdx) - 1
-                # In case the word is longer then the terminal, and the word doesn't end until the \r or the end of the string
-            temp = text[currentIdx : spaceLoc + 1]
-            print(temp, end="")
-            currentIdx = spaceLoc + 1
-            charLeft = termLen - (len(temp) % termLen)
-            lines += len(temp) // termLen
-            # If the word is longer then the terminal, print the entire thing with no \n, in case the terminal ever gets bigger
+        # endregion
 
-
-        # XXX \r processing, Priority: High XXX
+        # region XXX \r (in end) processing, Priority: Highest XXX
         if currentIdx == backIdx:
             backTriggered = True
             backIdx = 999999
@@ -231,18 +275,91 @@ def fullPrint(*args, end="\n", modeOverride=None):
                 raise Exception("fullPrint has reached a case it cannot handle when processing \\r. Please contact us if you see this error.")
             else:
                 for _ in range(lines):
-                    print("\033[F", end="")
+                    print("\033M", end="")
                 print("\r", end="")
                 charLeft = termLen
                 lines = 0
-        
 
-        # XXX Checks if it's done printing XXX
+        # endregion
+
+        # region XXX Near finish / finish checking, Priority: Medium XXX/
         if currentIdx == len(text):
             break
+        if charLeft > len(text) - currentIdx:
+            print(text[currentIdx:], end="")
+            if offset != None:
+                if cursorCheck:
+                    x, _ = _cursorPos()
+                    offset = x - 1 - startPos
+                else:
+                    offset = len(text) - currentIdx
+            break
 
-    # Only triggers when the snippet above wasn't run
+        # endregion
+
+        # region XXX Space processing, Priority: Low XXX
+        spaceLoc = text.rfind(" ", currentIdx, min(currentIdx + charLeft, backIdx))
+        if spaceLoc != -1:
+            if spaceLoc == min(currentIdx + charLeft, backIdx) - 1:
+                print(text[currentIdx : spaceLoc], end="")
+            else:
+                print(text[currentIdx : spaceLoc] + rmChar, end="")
+            charLeft = charLeft - len(text[currentIdx : spaceLoc])
+            currentIdx = spaceLoc + 1
+            if cursorCheck:
+                x, _ = _cursorPos()
+                if x - 1 != termLen - charLeft:
+                    endWordCheck = True
+                    charLeft = termLen - x
+                    print(sep, end="")
+                    continue
+            offset = None
+            endWordCheck = False
+            print("\n", end="")
+            charLeft = termLen
+            lines += 1
+            # Gets the location of the last space on the line, and moves the word to the next one
+        else:
+            offset = None
+            if endWordCheck:
+                print("\n", end="")
+                charLeft = termLen
+                lines += 1
+                endWordCheck = False
+                continue
+            spaceLoc = text.find(" ", currentIdx, min(len(text), backIdx))
+            if spaceLoc == -1:
+                # In case the word is longer then the terminal, and the word doesn't end until the \r or the end of the string
+                downLoc = text.find("\n", currentIdx, len(text))
+                backLoc = text.find("\r", currentIdx, len(text))
+
+                downLoc = downLoc if downLoc >= 0 else 999999
+                backLoc = backLoc if backLoc >= 0 else 999999
+
+                spaceLoc = min(downLoc, backLoc, len(text), backIdx) - 1
+
+            temp = text[currentIdx : spaceLoc + 1]
+            if maintainWords:
+                print(temp, end="")
+            else: # TODO: Implement cursor checking before going down (keep ANSI in mind). Maybe just have a black box ~8 characters after a sequence?
+                split = currentIdx + charLeft - 1
+                print(text[currentIdx : split] + "\n" + text[split : spaceLoc + 1], end="")
+            currentIdx = spaceLoc + 1
+            if cursorCheck:
+                x, _ = _cursorPos()
+                charLeft = termLen - x
+            else:
+                charLeft = termLen - (len(temp) % termLen)
+            lines += len(temp) // termLen
+            endWordCheck = True
+            # If the word is longer then the terminal, print the entire thing with no \n, in case the terminal ever gets bigger
+    
+    # endregion
+
+    # Only triggers when the \r code isn't run
     if not backTriggered:
-        raise Exception("fullPrint hasn't handled \\r correctly. Please contact us if you see this error.")
+        raise Exception(
+            "fullPrint hasn't handled \\r correctly. Please contact us if you see this error."
+        )
     relConsolePos += lines
-    return(lines)
+    return(types.SimpleNamespace(y=lines, x=offset))
